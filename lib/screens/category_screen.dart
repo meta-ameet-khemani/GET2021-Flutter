a1fd06c4dae435f8f46de889dd6ce72171b7ae8c
AMEET-KHEMANI\ameet.khemani_metacu <ameet.khemani@metacube.com>
2021-05-03 12:21:54 +0530

    1. Added a homepage screen i.e Categories screen: which will show all categories in grid view

---
+++ lib/screens/category_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import '../screens/category_item.dart';
+import '../temp_asset/category_items.dart';
+
+class CategoryScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Meal App'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: GridView(
+          children: CATEGORY_ITEM.map((catItem) =>
+            CategoryItem(catItem.title, catItem.color)
+          ).toList(),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200, // space to be taken by each grid item
+            childAspectRatio: 3 / 2, // space between children
+            crossAxisSpacing: 20, // space between column and grid item
+            mainAxisSpacing: 20, // space between row and grid item
+          ),
+        ),
+      ),
+    );
+  }
+}
