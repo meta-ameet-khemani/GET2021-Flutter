43c28494275a06a67f2648ddae36fd7ab12ed61d
AMEET-KHEMANI\ameet.khemani_metacu <ameet.khemani@metacube.com>
2021-05-24 10:32:54 +0530

    Local Storage Eample using shared preferences

---
+++ android/app/src/main/kotlin/com/example/share_preferences_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.share_preferences_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,176 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  SharedPreferences sharedPreferences;
+
+  Future<void> initialiseSharedPreferences() async {
+    this.sharedPreferences = await SharedPreferences.getInstance();
+    this.sharedPreferences.setString('name', 'Ameet Khemani');
+  }
+
+  Future<void> add(String key, String value, {bool isEdit = false}) async {
+    if (this.sharedPreferences != null) {
+      // isEdit ? print('To Update') : print('To Add');
+      if (isEdit) {
+        await this.sharedPreferences.setString(key, value);
+      } else {
+        await this.sharedPreferences.setString(key, value);
+      }
+      setState(() {});
+    }
+  }
+
+  // void update(String key, String value) {
+  //   if (this.sharedPreferences != null) {
+  //     this.sharedPreferences.setString(key, value);
+  //   }
+  // }
+
+  String convertPerson(Person person) {
+    return jsonEncode(person.toMap());
+  }
+
+  void delete(String key) {
+    if (this.sharedPreferences != null) {
+      this.sharedPreferences.remove(key);
+    }
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    initialiseSharedPreferences().whenComplete(() {
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    add(
+        'person',
+        convertPerson(
+            Person(name: 'John Doe', age: 35, address: 'California')));
+    Person person = this.sharedPreferences == null
+        ? null
+        : Person.fromJson(
+            jsonDecode(this.sharedPreferences?.getString('person')));
+    final name = this.sharedPreferences?.getString('name');
+    final phone = this.sharedPreferences?.getString('phone');
+    String centerText = 'name : $name \n person.name ${person?.name}';
+    centerText = phone == null ? centerText : centerText + '\nphone: $phone';
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              centerText,
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: SingleChildScrollView(
+        child: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FloatingActionButton(
+                onPressed: () {
+                  add('phone', 'Iphone 11');
+                },
+                tooltip: 'Increment',
+                child: Icon(
+                  Icons.add,
+                  size: 32,
+                ),
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  add('name', 'John', isEdit: true);
+                },
+                tooltip: 'Update',
+                child: Icon(Icons.edit),
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  add('name', 'John Doe', isEdit: true);
+                },
+                tooltip: 'Refresh',
+                child: Icon(
+                  Icons.refresh,
+                  size: 30,
+                ),
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  delete('phone');
+                },
+                tooltip: 'Delete',
+                child: Icon(Icons.delete),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Person {
+  String name;
+  int age;
+  String address;
+
+  Person({this.name, this.age, this.address});
+
+  factory Person.fromJson(Map<String, dynamic> personMap) {
+    return Person(
+      name: personMap['name'],
+      age: personMap['age'],
+      address: personMap['address'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {'name': name, 'age': age, 'address': address};
+  }
+}
