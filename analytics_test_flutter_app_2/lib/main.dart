7f2e651cff64d39eb071bb0465098dacbb23f089
AMEET-KHEMANI\ameet.khemani_metacu <ameet.khemani@metacube.com>
2021-07-08 17:59:50 +0530

    Added plugin for firebase analytics. Just a basic implementation i.e there is not everything there

---
+++ analytics_test_flutter_app_2/lib/main.dart
@@ -0,0 +1,288 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+  FirebaseAnalytics analytics = FirebaseAnalytics();
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() async {
+    await widget.analytics.logEvent(name: 'increment_event');
+    await _sendAnalyticsEvent();
+    await _testSetUserId();
+    await _testSetCurrentScreen();
+    await _testSetAnalyticsCollectionEnabled();
+    await _testSetSessionTimeoutDuration();
+    await _testSetUserProperty();
+    await _testAllEventTypes();
+    setState(() {
+      _counter++;
+    });
+  }
+
+  Future<void> _sendAnalyticsEvent() async {
+    await widget.analytics.logEvent(
+      name: 'test_event',
+      parameters: <String, dynamic>{
+        'string': 'string',
+        'int': 42,
+        'long': 12345678910,
+        'double': 42.0,
+        'bool': true,
+      },
+    );
+    setMessage('logEvent succeeded');
+  }
+
+  Future<void> _testSetUserId() async {
+    await widget.analytics.setUserId('some-user');
+    setMessage('setUserId succeeded');
+  }
+
+  Future<void> _testSetCurrentScreen() async {
+    await widget.analytics.setCurrentScreen(
+      screenName: 'Analytics Demo',
+      screenClassOverride: 'AnalyticsDemo',
+    );
+    setMessage('setCurrentScreen succeeded');
+  }
+
+  Future<void> _testSetAnalyticsCollectionEnabled() async {
+    await widget.analytics.setAnalyticsCollectionEnabled(false);
+    await widget.analytics.setAnalyticsCollectionEnabled(true);
+    setMessage('setAnalyticsCollectionEnabled succeeded');
+  }
+
+  Future<void> _testSetSessionTimeoutDuration() async {
+    await widget.analytics.android?.setSessionTimeoutDuration(2000000);
+    setMessage('setSessionTimeoutDuration succeeded');
+  }
+
+  Future<void> _testSetUserProperty() async {
+    await widget.analytics.setUserProperty(name: 'regular', value: 'indeed');
+    setMessage('setUserProperty succeeded');
+  }
+
+  Future<void> _testAllEventTypes() async {
+    await widget.analytics.logAddPaymentInfo();
+    await widget.analytics.logAddToCart(
+      currency: 'USD',
+      value: 123,
+      itemId: 'test item id',
+      itemName: 'test item name',
+      itemCategory: 'test item category',
+      quantity: 5,
+      price: 24,
+      origin: 'test origin',
+      itemLocationId: 'test location id',
+      destination: 'test destination',
+      startDate: '2015-09-14',
+      endDate: '2015-09-17',
+    );
+    await widget.analytics.logAddToWishlist(
+      itemId: 'test item id',
+      itemName: 'test item name',
+      itemCategory: 'test item category',
+      quantity: 5,
+      price: 24,
+      value: 123,
+      currency: 'USD',
+      itemLocationId: 'test location id',
+    );
+    await widget.analytics.logAppOpen();
+    await widget.analytics.logBeginCheckout(
+      value: 123,
+      currency: 'USD',
+      transactionId: 'test tx id',
+      numberOfNights: 2,
+      numberOfRooms: 3,
+      numberOfPassengers: 4,
+      origin: 'test origin',
+      destination: 'test destination',
+      startDate: '2015-09-14',
+      endDate: '2015-09-17',
+      travelClass: 'test travel class',
+    );
+    await widget.analytics.logCampaignDetails(
+      source: 'test source',
+      medium: 'test medium',
+      campaign: 'test campaign',
+      term: 'test term',
+      content: 'test content',
+      aclid: 'test aclid',
+      cp1: 'test cp1',
+    );
+    await widget.analytics.logEarnVirtualCurrency(
+      virtualCurrencyName: 'bitcoin',
+      value: 345.66,
+    );
+    await widget.analytics.logEcommercePurchase(
+      currency: 'USD',
+      value: 432.45,
+      transactionId: 'test tx id',
+      tax: 3.45,
+      shipping: 5.67,
+      coupon: 'test coupon',
+      location: 'test location',
+      numberOfNights: 3,
+      numberOfRooms: 4,
+      numberOfPassengers: 5,
+      origin: 'test origin',
+      destination: 'test destination',
+      startDate: '2015-09-13',
+      endDate: '2015-09-14',
+      travelClass: 'test travel class',
+    );
+    await widget.analytics.logGenerateLead(
+      currency: 'USD',
+      value: 123.45,
+    );
+    await widget.analytics.logJoinGroup(
+      groupId: 'test group id',
+    );
+    await widget.analytics.logLevelUp(
+      level: 5,
+      character: 'witch doctor',
+    );
+    await widget.analytics.logLogin();
+    await widget.analytics.logPostScore(
+      score: 1000000,
+      level: 70,
+      character: 'tiefling cleric',
+    );
+    await widget.analytics.logPresentOffer(
+      itemId: 'test item id',
+      itemName: 'test item name',
+      itemCategory: 'test item category',
+      quantity: 6,
+      price: 3.45,
+      value: 67.8,
+      currency: 'USD',
+      itemLocationId: 'test item location id',
+    );
+    await widget.analytics.logPurchaseRefund(
+      currency: 'USD',
+      value: 45.67,
+      transactionId: 'test tx id',
+    );
+    await widget.analytics.logSearch(
+      searchTerm: 'hotel',
+      numberOfNights: 2,
+      numberOfRooms: 1,
+      numberOfPassengers: 3,
+      origin: 'test origin',
+      destination: 'test destination',
+      startDate: '2015-09-14',
+      endDate: '2015-09-16',
+      travelClass: 'test travel class',
+    );
+    await widget.analytics.logSelectContent(
+      contentType: 'test content type',
+      itemId: 'test item id',
+    );
+    await widget.analytics.logShare(
+        contentType: 'test content type',
+        itemId: 'test item id',
+        method: 'facebook');
+    await widget.analytics.logSignUp(
+      signUpMethod: 'test sign up method',
+    );
+    await widget.analytics.logSpendVirtualCurrency(
+      itemName: 'test item name',
+      virtualCurrencyName: 'bitcoin',
+      value: 34,
+    );
+    await widget.analytics.logTutorialBegin();
+    await widget.analytics.logTutorialComplete();
+    await widget.analytics.logUnlockAchievement(id: 'all Firebase API covered');
+    await widget.analytics.logViewItem(
+      itemId: 'test item id',
+      itemName: 'test item name',
+      itemCategory: 'test item category',
+      itemLocationId: 'test item location id',
+      price: 3.45,
+      quantity: 6,
+      currency: 'USD',
+      value: 67.8,
+      flightNumber: 'test flight number',
+      numberOfPassengers: 3,
+      numberOfRooms: 1,
+      numberOfNights: 2,
+      origin: 'test origin',
+      destination: 'test destination',
+      startDate: '2015-09-14',
+      endDate: '2015-09-15',
+      searchTerm: 'test search term',
+      travelClass: 'test travel class',
+    );
+    await widget.analytics.logViewItemList(
+      itemCategory: 'test item category',
+    );
+    await widget.analytics.logViewSearchResults(
+      searchTerm: 'test search term',
+    );
+    setMessage('All standard events logged successfully');
+  }
+
+  void setMessage(String message) {
+    setState(() {
+      // _message = message;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,68 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<firebase_core/FLTFirebaseCorePlugin.h>)
+#import <firebase_core/FLTFirebaseCorePlugin.h>
+#else
+@import firebase_core;
+#endif
+
+#if __has_include(<firebase_remote_config/FLTFirebaseRemoteConfigPlugin.h>)
+#import <firebase_remote_config/FLTFirebaseRemoteConfigPlugin.h>
+#else
+@import firebase_remote_config;
+#endif
+
+#if __has_include(<flutter_local_notifications/FlutterLocalNotificationsPlugin.h>)
+#import <flutter_local_notifications/FlutterLocalNotificationsPlugin.h>
+#else
+@import flutter_local_notifications;
+#endif
+
+#if __has_include(<google_maps_flutter/FLTGoogleMapsPlugin.h>)
+#import <google_maps_flutter/FLTGoogleMapsPlugin.h>
+#else
+@import google_maps_flutter;
+#endif
+
+#if __has_include(<image_picker/FLTImagePickerPlugin.h>)
+#import <image_picker/FLTImagePickerPlugin.h>
+#else
+@import image_picker;
+#endif
+
+#if __has_include(<path_provider/FLTPathProviderPlugin.h>)
+#import <path_provider/FLTPathProviderPlugin.h>
+#else
+@import path_provider;
+#endif
+
+#if __has_include(<shared_preferences/FLTSharedPreferencesPlugin.h>)
+#import <shared_preferences/FLTSharedPreferencesPlugin.h>
+#else
+@import shared_preferences;
+#endif
+
+#if __has_include(<url_launcher/FLTURLLauncherPlugin.h>)
+#import <url_launcher/FLTURLLauncherPlugin.h>
+#else
+@import url_launcher;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTFirebaseCorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseCorePlugin"]];
+  [FLTFirebaseRemoteConfigPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseRemoteConfigPlugin"]];
+  [FlutterLocalNotificationsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterLocalNotificationsPlugin"]];
+  [FLTGoogleMapsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTGoogleMapsPlugin"]];
+  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+  [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
+  [FLTURLLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTURLLauncherPlugin"]];
+}
+
+@end
